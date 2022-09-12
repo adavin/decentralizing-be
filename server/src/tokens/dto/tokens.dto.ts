06b922c796c1d4a14387fe8c02d8a22ec86240fb
adavin <alex@alexdavin.com>
2022-09-12 05:38:13 -0700

    Missing NewUsertoken type

--- server/src/tokens/dto/tokens.dto.ts
+++ server/src/tokens/dto/tokens.dto.ts
@@ -1,6 +1,29 @@
 /* eslint-disable prettier/prettier */
 import { UserToken } from '../tokens.entity'
 
+export class NewUserTokenDto {
+  user_id: number;
+  api_token: string;
+  token_address: string;
+  token_name: string;
+  token_symbol: string;
+  initial_supply: string;
+  txid: string;
+}
+
+export class NewUserTokenResponseDto {
+  result: boolean;
+  response: string;
+  txid: string;
+  errorCode: number;
+  constructor() {
+    this.txid = ''
+    this.errorCode = 0
+    this.response = ''
+    this.result = false
+  }
+}
+
 export class GetUserTokensDto {
   user_id: number
   api_token: string
